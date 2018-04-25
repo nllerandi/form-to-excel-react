9468a5bb8890e18f0db08745523fc669ecf8b0b1
Nicholas Llerandi <nicholasllerandi@Nicholass-Air.fios-router.home>
2018-04-24 21:14:36 -0400

    914

--- src/App.js
+++ src/App.js
@@ -10,43 +10,82 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to VZMediaOps</h1>
         </header>
-        <form id="fiosForm">
-          <div class="container-fluid">
-            <table class="table table-striped">
-              <thead>
+          <form id="fiosForm">
+              <table class="table table-striped">
+                  <thead>
+                    <tr>
+                    <th>Tactic</th>
+                    <th>LOB</th>
+                    <th>Type</th>
+                    <th>Project Code</th>
+                    <th>Group</th>
+                    <th>In-Market Date</th>
+                    <th>Platform</th>
+                    <th>Desc</th>
+                    <th>Misc</th>
+                    <th>URL</th>
+                    </tr>
+                  </thead>
+                <tbody>
                 <tr>
-                  <th>Tactic</th>
-                  <th>Line of Business</th>
-                  <th>Campaign Type</th>
-                  <th>Project Code</th>
-                  <th>Group</th>
-                  <th>In Market Date</th>
-                  <th>Platform</th>
-                  <th>Description</th>
-                  <th>Misc</th>
-                  <th>Landing Page URL</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                <tr>
-                  <div class="form-row">
                     <td>
-                    <div class="form-group col-sm-12">
-                        <select>
-                            <option value="afc">AFC</option>
-                            <option value="tva">TVA</option>
-                            <option value="bac">BAC</option>
-                            <option value="dmo">DMO</option>
+                        <select name="tactic">
+                            <option value="AFC">Affiliate</option>
+                            <option value="BAC">Banner Ad</option>
+                            <option value="EMC">Email</option>
+                            <option value="OLV">Online Video</option>
                         </select>
-                    </div>
                     </td>
-                  </div>
+                    <td>
+                        <select name="lob">
+                            <option value="CON">Consumer</option>
+                            <option value="SMB">Small Business</option>
+                        </select>
+                    </td>
+                    <td>
+                        <select name="type">
+                            <option value="ACQ">Acquisition</option>
+                            <option value="CRM">CRM</option>
+                            <option value="PRS">Prospect</option>
+                            <option value="AWR">Awareness</option>
+                            <option value="OTH">Other</option>
+                        </select>
+                    </td>
+                    <td>
+                        <input id="projectCode" type="text" size="6"/>
+                    </td>
+                    <td>
+                        <select name="group">
+                            <option value="OF">140</option>
+                            <option value="AA">AdAsia</option>
+                            <option value="AM">Ascend Marketing</option>
+                            <option value="BG">Bite Global</option>
+                        </select>
+                    </td>
+                    <td>
+                        <input id="inMarketDate" type="date" />
+                    </td>
+                    <td>
+                        <select name="platform">
+                            <option value="NA">NA</option>
+                            <option value="FBK">Facebook</option>
+                            <option value="TWT">Twitter</option>
+                            <option value="GGL">Google</option>
+                        </select>
+                    </td>
+                    <td>
+                        <input id="description" type="text" maxlength="10" size="10"/>
+                    </td>
+                    <td>
+                        <input id="misc" type="text" maxlength="10" size="10"/>
+                    </td>
+                    <td>
+                        <input id="url" type="text" size="10" pattern="https://.+" title="Must be a secure URL - use https://"/>
+                    </td>
                 </tr>
-              </tbody>
-
+                </tbody>
             </table>
-          </div> {/*container-fluid*/}
+            <button type="submit" class="btn btn-primary" id="submit-button">Submit</button>
         </form>
       </div>
     );
